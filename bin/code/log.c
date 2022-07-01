866d0f99382837cfdfee498e95609df243b567d3
Seemingly <2781343172@qq.com>
2022-07-01 23:27:32 +0800

    enter

--- bin/code/log.c
+++ bin/code/log.c
@@ -1,3 +1,18 @@
 #include"log.h"
 #include <process.h>
 #include <stdlib.h>
+
+bool log_debug_print_char(char c)
+{
+    printf("%c",c);
+    return true;
+}
+
+bool log_debug_print_string(char* str)
+{
+    for(int i=0;str[i]!='\0';i++)
+    {
+        log_debug_print_char(str[i]);
+    }
+    return true;
+}
